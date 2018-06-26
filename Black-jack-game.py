11387251f92742bd83f433d038f65debfbdf9abd
kaipietila <kai.m.pietila@gmail.com>
2018-06-26 21:02:53 +0300

    added new logic
    Whole new logic and classes

--- Black-jack-game.py
+++ Black-jack-game.py
@@ -1,47 +1,58 @@
+import random
+
+suits = ('hearts', 'diamonds', 'clubs', 'spades')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+playing = True
+
 class Card:
     '''
     This class defines the cards, deals a card and checks its validity
     '''
-    def __init__(self,suit=0,value=0):
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.value = value
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return (f'{self.rank} of {self.suit}')
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                card = Card(suit,rank)
+                self.deck.append(card)
+        self.shuffle()
+
+    def shuffle(self):
+        random.shuffle(self.deck)
 
     def deal(self):
-        from random import randint  
-        '''
-        generates a card with a random suit and a random value as dealing from a card deck
-        '''
-        self.suit = randint(1,4)
-        self.value = randint(1,14)
-        print(self.suit,self.value)
-        return (self.suit,self.value)
+        return self.deck.pop(0)
 
-    def check_valid_card(self,card,player_hand,dealer_hand):
-        '''
-        checks if the card is already in the player or the dealers hand, if its in a hand then card is invalid
-        '''
-        if card in player_hand:
-            return False
-        elif card in dealer_hand:
-            return False
-        else:
-            print("card is not in any hand")
-            print(card)
-            return True
+class Hand:
 
-    def card_actual_value(self):
-        '''
-        Gives the true value of the card. For cards 1-9 returns the value given. For 10,jack,queen,king returns value 10
-        and for aces returns value 11.
-        '''
-        if self.value < 10:
-            print(self.value)
-            return self.value
-        elif self.value >= 10 and self.value < 14:
-            print(10)
-            return 10
+    def __init__(self)
+        self.cards = []  
+        self.value = 0   
+    
+    def add_card(self,card):
+        self.cards.append(card)
+        if card.rank != 'Ace':
+            self.value += card.value
         else:
-            return 11
+            self.adjust_for_ace()
+    
+    def adjust_for_ace(self):
+        if self.value <= 10:
+            self.value += 11
+        else:
+            self.value += 1
 
 
 class Wallet:
@@ -55,14 +66,6 @@ class Wallet:
         self.amount += amount
         return self.amount
     
-    def make_bet(self):
-        bet_amount= int(input(f"How much would you like to bet? Your wallet contains {self.amount}.\n"))
-        if bet_amount > self.amount:
-            print("The bet was too large!")
-            return False
-        else:
-            return bet_amount
-    
     def check_bet(self, bet_amount, win = False):
         if win:
             self.amount += bet_amount
@@ -74,71 +77,56 @@ class Wallet:
     def show_wallet(self):
         return self.amount
 
+class Player:
 
-def hit_card(player_card, player_hand, dealer_hand):
-    if card.check_valid_card(player_card, player_hand, dealer_hand):
-        player_hand.append(player_card)
-        player_turn = False
-        player_hand_value += card.card_actual_value()
-        card_count += 1
+    def __init__(self, wallet, hand):
+        self.wallet = wallet
+        self.hand = hand
     
 
-def win_check():
-    '''
-    To check if the player_hand_value is 21 or over and then to compare that to the dealer_hand_value.
-    If player_hand_value is below 21 the dealer will draw cards until same or over player_hand_value. 
-    If dealer_hand_value exceeds 21 the player wins.
-    '''
-    pass
+def make_bet(self, wallet):
+    bet_amount= int(input(f"How much would you like to bet? Your wallet contains {wallet.amount}.\n"))
+    if bet_amount > wallet.amount:
+        print("The bet was too large!")
+        return False
+    else:
+        return bet_amount
+
+
+def hit_card(deck, hand):
+    card = deck.deal()
+    hand.add_card(card)
+       
+def hit_or_stand(deck, hand):
+    global playing  # to control an upcoming while loop
+    if playing:
+        hit_card(deck, hand)
+
+def show_some(player,dealer):
+    print(f'Player hand value {player.value}')
+    print (f'Dealer hand value {dealer.value - dealer.cards[0].value}')
+    
+def show_all(player,dealer):
+    print (f'Player hand value {player.value}')
+    print (f'Dealer hand value {dealer.value}')
+
+def player_busts(player):
+    if player.hand.value > 21:
+        return True
+    else:
+        return False
+
+def player_wins(player1, player2):
+    if player2.hand.value < player1.hand.value <= 21:
+        return True
+    else:
+        return False
+    
+def push():
+    print("its a tie")
+
 
 def playing_game():
 
-    player_hand = []
-    dealer_hand = []
-    player_hand_value = 0
-    dealer_hand_value = 0
-    game_end = False
-    stop = False
-    player_turn = True
-    card_count = 0
-    '''
-    Need to make a new def called deal_card that uses classes and deals one call on demand. since the game
-    logic goes two cards to both and then the player gets to choose hit or stop until he stops.another while loop until stop
-    '''
 
-    while game_end is not True:
-        if player_turn:
-            card = Card()
-            player_card = card.deal()
-            hit_card(player_card, player_hand, dealer_hand)
-            elif card_count >= 4:
-                choice = input("Do you want to hit or stop? ") 
-                if choice is "hit":
-                    continue
-                else:
-
-            else:
-                continue
-        else:
-            card = Card()
-            dealer_card = card.deal()
-            if card.check_valid_card(dealer_card, player_hand, dealer_hand):
-                dealer_hand.append(dealer_card)
-                player_turn = True
-                dealer_hand_value += card.card_actual_value()
-                card_count += 1
-'''        
-if __name__ == "__main__":
-    playing_game()
-'''
-
-player_hand = []
-dealer_hand = []
-
-card = Card()
-assigned_card = card.deal()
-card.card_actual_value()
-card.check_valid_card(assigned_card, player_hand, dealer_hand)
-
-
-
+    while 
